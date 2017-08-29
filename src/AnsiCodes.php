7297ddbb3a3aa8703a0413c487e2607b074aaee6
bartikowskiw <bartikowskiw@missouri.edu>
2017-08-29 11:30:25 -0500

    Own class for ANSI control string constants.

---
+++ src/AnsiCodes.php
@@ -0,0 +1,20 @@
+<?php
+
+declare( strict_types = 1 );
+namespace Umlts\MarcToolset;
+
+/**
+ * ANSI control strings
+ */
+class AnsiCodes {
+
+    const red = "\e[31m";
+    const yellow = "\e[33m";
+    const magenta = "\e[95m";
+
+    const bold = "\e[1m";
+    const dim = "\e[2m";
+    const underline = "\e[4m";
+    const negative = "\e[7m";
+    const reset = "\e[0m";
+}
