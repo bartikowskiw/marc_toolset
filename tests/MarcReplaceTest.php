22f6fd550b3c9f24aa089d1f6d5c07bc5a798d6c
bartikowskiw <bartikowskiw@missouri.edu>
2017-09-06 16:28:25 -0500

    Added tests.

---
+++ tests/MarcReplaceTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare( strict_types = 1 );
+
+use PHPUnit\Framework\TestCase;
+use Umlts\MarcToolset\MarcReplace;
+use Umlts\MarcToolset\MarcMask;
+
+/**
+ * @covers Umlts\MarcToolset\MarcReplace
+ */
+final class MarcReplaceTest extends TestCase {
+
+    public function testCanBeCreated() {
+        $mr = new MarcReplace(
+            __DIR__ . '/data/random.mrc',
+            new MarcMask(),
+            ''
+        );
+        $this->assertInstanceOf( MarcReplace::class, $mr );
+    }
+
+    public function testReplace() {
+        $mr = new MarcReplace(
+            __DIR__ . '/data/random.mrc',
+            new MarcMask( '65.', '.', '0', '.', 'beef' ),
+            'pork'
+        );
+
+        $record = $mr->next();
+
+        $content = (string) $record;
+
+        $this->assertEquals( true, !empty( $content ) );
+        $this->assertNotEquals( false, strpos( $content, 'pork' ) );
+    }
+
+}
