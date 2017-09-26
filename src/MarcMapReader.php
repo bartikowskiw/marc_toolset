528b0e208dae144e4ec0a91abd0000a380569a1e
bartikowskiw <bartikowskiw@missouri.edu>
2017-09-26 13:38:02 -0500

    Added MarcRecordInvalidException to readRecord() method.

--- src/MarcMapReader.php
+++ src/MarcMapReader.php
@@ -6,6 +6,7 @@ namespace Umlts\MarcToolset;
 
 use File\MARC;
 use Umlts\MarcToolset\MarcRecordNotFoundException;
+use Umlts\MarcToolset\MarcRecordInvalidException;
 
 /**
  * Looks up a MARC record by a key (usually OCLC / MARC Tag 001) and
@@ -84,14 +85,26 @@ class MarcMapReader {
      *   Open file pointer to the MARC file
      * @var int $fpos
      * @return File_MARC_Record
+     * @throws MarcRecordInvalidException
      */
     public static function readRecord( $fp, int $fpos ) : \File_MARC_Record {
 
         fseek( $fp, $fpos );
 
         $len = intval( fread( $fp, 5 ) );
+        if ( $len == 0 ) {
+            throw new MarcRecordInvalidException(
+                'MARC record invalid (File position: ' . number_format( $fpos ) . ' ).'
+            );
+        }
         fseek( $fp, -5, SEEK_CUR );
         $marc_binary = fread( $fp, $len );
+
+        if ( $marc_binary === FALSE ) {
+            throw new MarcRecordInvalidException(
+                'MARC record invalid (File position: ' . number_format( $fpos ) . ' ).'
+            );
+        }
 
         $marc = new \File_MARC( $marc_binary, \File_MARC::SOURCE_STRING );
 
