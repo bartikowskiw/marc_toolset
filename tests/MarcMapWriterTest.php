6564cd99ce6108c9fb0a788b72c801821101ab29
bartikowskiw <bartikowskiw@missouri.edu>
2018-01-17 10:26:04 -0600

    Added test for custom key creators

--- tests/MarcMapWriterTest.php
+++ tests/MarcMapWriterTest.php
@@ -4,6 +4,21 @@ declare( strict_types = 1 );
 
 use PHPUnit\Framework\TestCase;
 use Umlts\MarcToolset\MarcMapWriter;
+use Umlts\MarcToolset\MarcMapKeyCreator;
+
+class customKeyCreator implements MarcMapKeyCreator {
+    
+    static function getKeys( \File_MARC_Record $record ) : array {
+        if ( empty( $record->getField( '245' ) ) ) { return [ -1 ]; }
+        
+        $keys = [];
+        $fields = $record->getFields( '245' );
+        foreach ( $fields as $field ) {
+            $keys[] = $field->getSubfield( 'a' )->getData();
+        }
+        return $keys;
+    }
+}
 
 /**
  * @covers Umlts\MarcToolset\MarcMapWriter
@@ -23,6 +38,24 @@ final class MarcMapWriterTest extends TestCase {
         $result = $db->query( 'SELECT COUNT(*) AS count FROM `record`' );
         $data = $result->fetchArray( SQLITE3_ASSOC );
         $this->assertEquals( $data['count'], 100 );
+    }
+    
+    public function testCustomKeyCreator() {
+        $db = new SQLite3( ':memory:' );
+        $mw = new MarcMapWriter( __DIR__ . '/data/random.mrc', $db );
+        $mw->setKeyCreator( new customKeyCreator() );
+        $mw->map();
+    
+        $result = $db->query( 'SELECT COUNT(*) AS count FROM `record`' );
+        $data = $result->fetchArray( SQLITE3_ASSOC );
+        $this->assertEquals( $data['count'], 100 );
+        
+        $result = $db->query( '
+            SELECT COUNT(*) AS count FROM `record` 
+            WHERE `key` = \'Clostridium difficile on U.S. beef cow-calf operations.\'
+        ' );
+        $data = $result->fetchArray( SQLITE3_ASSOC );
+        $this->assertEquals( $data['count'], 1 );
     }
 
 }
